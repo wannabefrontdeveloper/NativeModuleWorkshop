728fbd0d2990bfe4f38c25deac3b7e7b8b357b85
wannabefrontdeveloper <giwonk@naver.com>
2024-03-02 12:55:11 +0900

    코드추가 getConstants 메서드를 만들어서 상수들을 지닌 Map을 생성하였습니다.

--- android/app/src/main/java/com/nativemoduleworkshop/ToastModule.java
+++ android/app/src/main/java/com/nativemoduleworkshop/ToastModule.java
@@ -5,6 +5,9 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ToastModule extends ReactContextBaseJavaModule {
   ToastModule(ReactApplicationContext context) {
     super(context);
@@ -20,6 +23,14 @@ public class ToastModule extends ReactContextBaseJavaModule {
     ReactApplicationContext context = getReactApplicationContext();
     Toast toast = Toast.makeText(context, message, duration);
     toast.show();
+  }
+
+  @Override
+  public Map<String, Object> getConstants() {
+    final Map<String, Object> constants = new HashMap<>();
+    constants.put("SHORT", Toast.LENGTH_SHORT);
+    constants.put("LONG", Toast.LENGTH_LONG);
+    return constants;
   }
 
 }
