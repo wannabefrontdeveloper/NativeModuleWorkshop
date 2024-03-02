4e2aa129b31833b9debb1ccc6e85138524df8c96
wannabefrontdeveloper <giwonk@naver.com>
2024-03-02 14:16:20 +0900

    코드수정 세가지 버튼과 하나의 텍스트로 화면을 구성하였습니다.

--- App.js
+++ App.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import {Button, SafeAreaView} from 'react-native';
-import ToastModule from './Toast';
+import React, {useState} from 'react';
+import {Button, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {getBrightness, setBrightness} from './Brightness';
 
 function App() {
-  const onPress = () => {
-    ToastModule.show('Hello World', ToastModule.SHORT);
+  const [value, setValue] = useState(-1);
+  const onPress = async () => {
+    const brightness = await getBrightness();
+    setValue(brightness);
+  };
+
+  const onPressLow = () => {
+    setBrightness(0.25);
+  };
+
+  const onPressHigh = () => {
+    setBrightness(1);
   };
 
   return (
-    <SafeAreaView>
-      <Button title="Press me" onPress={onPress} />
+    <SafeAreaView style={styles.block}>
+      <Button title="Update Brightness" onPress={onPress} />
+      <View style={styles.textWrapper}>
+        <Text style={styles.text}>{value}</Text>
+      </View>
+      <Button title="Low Brightness" onPress={onPressLow} />
+      <Button title="High Brightness" onPress={onPressHigh} />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+  },
+  textWrapper: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+  },
+  text: {
+    fontSize: 64,
+  },
+});
 
 export default App;
