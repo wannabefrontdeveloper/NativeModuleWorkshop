c9b87d4f37f8f6761fdcdc8414aa13036f3ce150
wannabefrontdeveloper <giwonk@naver.com>
2024-03-02 12:48:20 +0900

    코드추가 네이티브 모듈을 만들기 위해서 리액트 네이티브의 세가지 클래스를 불러오고, ReactContextBaseModule을 상속하고, getName 메서드를 오버라이딩 하였습니다.

--- android/app/src/main/java/com/nativemoduleworkshop/ToastModule.java
+++ android/app/src/main/java/com/nativemoduleworkshop/ToastModule.java
@@ -1,4 +1,17 @@
 package com.nativemoduleworkshop;
 
-public class ToastModule {
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+
+public class ToastModule extends ReactContextBaseJavaModule {
+  ToastModule(ReactApplicationContext context) {
+    super(context);
+  }
+
+  @Override
+  public String getName() {
+    return "ToastModule";
+  }
+
 }
